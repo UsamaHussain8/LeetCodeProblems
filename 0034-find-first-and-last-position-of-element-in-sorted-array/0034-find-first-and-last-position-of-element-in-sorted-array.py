39ff4fb6e60d13737a705f79aac8e6020b156cec
UsamaHussain8 <usamahussain00@gmail.com>
2024-08-13 23:36:23 +0500

    Time: 77 ms (48.25%), Space: 17.9 MB (34.58%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,29 +1,30 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-      if len(nums) == 0:
-        return [-1, -1]
+    def binarySearch(self, nums, target: int, left_present: bool):
+        left: int = 0
+        right: int = len(nums) - 1
+        idx: int = -1
 
-      indices = []
-
-      left: int = 0
-      right: int = len(nums) - 1
-      
-      while left <= right:
+        while left <= right:
             mid = (left + right) // 2
 
-            if nums[mid] == target:
-                indices.append(mid)
-                if (left + right <= 2) and (nums[mid - 1] != target or nums[mid + 1] != target):
-                    indices.append(mid)    
-                else:    
-                    indices.insert(0, left) if (nums[left] == target and left != mid)  else indices.append(right)
-                return indices
-
+            if nums[mid] < target:
+                left = mid + 1                
             elif nums[mid] > target:
                 right = mid - 1
-
             else:
-                left = mid + 1    
+                idx = mid    
+                if left_present == True:
+                    right = mid - 1
+                else:
+                    left = mid + 1    
 
-      if len(indices) == 0:
+        return idx            
+
+    def searchRange(self, nums, target: int):
+        if len(nums) == 0:
             return [-1, -1]
+
+        left = self.binarySearch(nums, target, True)
+        right = self.binarySearch(nums, target, False)  
+
+        return [left, right]
