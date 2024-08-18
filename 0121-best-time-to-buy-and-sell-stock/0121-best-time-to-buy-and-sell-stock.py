073cd77eafbf50d4038b3ad5487214be88e6e25e
UsamaHussain8 <usamahussain00@gmail.com>
2024-08-18 01:04:23 -0700

    Time: 739 ms (55.14%), Space: 27.4 MB (32.58%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,40 +4,12 @@ class Solution:
         if num_elements < 2:
             return 0
         
-        # min_element: int = prices[0]
-        # min_idx: int = 0
-
-        # # for i in range(1, num_elements):
-        # #     if prices[i] <= min_element:
-        # #         min_element = prices[i]
-        # #         min_idx = i 
-
-        # left: int = 0
-        # right: int = num_elements - 1
-        # while left != right:
-        #     if prices[left] <= prices[right]:
-        #         min_element = min(prices[left], min_element)      
-        #         left += 1
-        #     else:
-        #         min_element = min(prices[right], min_element)                
-        #         right -= 1  
-
-        # min_idx = prices.index(min_element)
-        # if min_idx == num_elements - 1:
-        #     return 0
-
-        # profit = min_element
-
-        # for i in range(min_idx + 1, num_elements):
-        #     if profit < prices[i] - min_element:
-        #         profit = prices[i] - min_element
+        price_for_buying: int = prices[0]
+        profit: int = 0
+        
+        for i in range(1, num_elements):
+            if prices[i] < price_for_buying:
+                price_for_buying = prices[i]
+            profit = max(prices[i] - price_for_buying, profit)    
             
-        # return profit
-
-        mid = num_elements // 2
-        min_element = min(prices[0: mid + 1])
-        min_idx = prices.index(min_element)
-        max_element = max(prices[min_idx: num_elements])
-
-        return max(max_element - min_element, 0)
-            
+        return profit        
