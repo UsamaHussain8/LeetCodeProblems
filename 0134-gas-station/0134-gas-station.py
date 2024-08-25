a7c6ae0e02fa4d11dd1d5ccbecc5cf7e1f8d8b65
UsamaHussain8 <usamahussain00@gmail.com>
2024-08-25 21:36:22 +0500

    Time: 787 ms (99.24%), Space: 22 MB (31.04%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -5,16 +5,12 @@ class Solution:
             return cyclic_index
 
         starting_station: int = 0
-        num_stations:int = len(gas)
-        net_fuel: int = 0
+        num_stations: int = len(gas)
         gas_in_tank: int = 0
 
         for i in range(0, num_stations):
-            net_fuel += gas[i] - cost[i]
             gas_in_tank += gas[i] - cost[i]
             if gas_in_tank < 0:
                 gas_in_tank = 0
                 starting_station = i + 1
-
-        cyclic_index = starting_station if net_fuel >= 0 else -1
-        return cyclic_index
+        return starting_station
