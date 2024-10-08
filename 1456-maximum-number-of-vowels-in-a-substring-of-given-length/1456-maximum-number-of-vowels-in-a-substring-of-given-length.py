62046310e0df6d4680c19465d45ef21a9e5309de
UsamaHussain8 <usamahussain00@gmail.com>
2024-10-08 23:27:33 +0500

    Time: 684 ms (5.01%), Space: 18.1 MB (5.84%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,15 +1,33 @@
-class Solution:
-    def maxVowels(self, s: str, k: int) -> int:
-        n_vowels = 0
+class Solution(object):
+    def maxVowels(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        currWindow, currLen, currVowelCount = [], 0, 0
         left = 0
+        n = len(s)
+        vowels = "AEIOUaeiou"
+        ret = 0
 
-        for right in range(left, len(s)):
-            substr = s[left: right + k]
-            n_vowels_cur = 0
-            for elem in substr:
-                if elem in ["a", "e", "i", "o", "u"]:
-                    n_vowels_cur += 1
-            n_vowels = max(n_vowels, n_vowels_cur)
-            left += 1
-
-        return n_vowels
+        for right in range(n):
+            letter = s[right]
+            currWindow.append(letter)
+            currLen += 1
+            if letter in vowels:
+                currVowelCount += 1
+            
+            while currLen > k:
+                removedLetter = s[left]
+                currWindow.remove(removedLetter)
+                currLen -= 1
+                if removedLetter in vowels:
+                    currVowelCount -= 1
+                left += 1
+            
+            # at this point satisfy the condition
+            if currLen == k:
+                ret = max(ret, currVowelCount)
+        
+        return ret
