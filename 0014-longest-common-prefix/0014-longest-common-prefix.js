16fa0d11c9a11146f1f4f4a57b560511a67863c8
UsamaHussain8 <usamahussain00@gmail.com>
2025-01-02 22:34:10 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let res = "";
+    if (strs.length == 0)
+        return res;
+
+    let min_length = 1000000;    
+    for (let idx = 0; idx < strs.length; ++idx) {
+        if (min_length > strs[idx].length)
+            min_length = strs[idx].length;
+    }    
+    
+    for (let idx = 0; idx < min_length; ++idx) {
+        for (let num_elem = 1; num_elem < strs.length; ++num_elem) {
+            if (strs[0][idx] != strs[num_elem][idx]) {            
+                return strs[0].substring(0, idx);
+            }            
+        }        
+    }
+
+    return strs[0];
+};
