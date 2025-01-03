9b8e07578d7e8a0e5cd1a42eecd036a0111af94c
UsamaHussain8 <usamahussain00@gmail.com>
2025-01-03 21:51:20 +0500

    Time: 3 ms (38.56%), Space: 50.1 MB (41.46%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -6,17 +6,10 @@ var longestCommonPrefix = function(strs) {
     let res = "";
     if (strs.length == 0)
         return res;
-
-    let min_length = 1000000;    
-    for (let idx = 0; idx < strs.length; ++idx) {
-        if (min_length > strs[idx].length)
-            min_length = strs[idx].length;
-    }    
     
     for (let idx = 0; idx < strs[0].length; ++idx) {
         for (let num_elem = 1; num_elem < strs.length; ++num_elem) {
-            if (strs[0][idx] != strs[num_elem][idx] || idx == strs[num_elem].length) {      
-                console.log("I went in!")      
+            if (strs[0][idx] != strs[num_elem][idx] || idx == strs[num_elem].length) {            
                 return strs[0].substring(0, idx);
             }            
         }        
